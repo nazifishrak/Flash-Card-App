2b60b315a3724d1face02bb4a57d606716fe384f
Nazif <nzfishrak60@gmail.com>
2022-08-03 16:45:46 +0600

    finished the flash card app

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 BACKGROUND_COLOR = "#B1DDC7"
 
 from tkinter import *
-from turtle import width
+from tkinter import messagebox
 import pandas
 import random
-
-data=pandas.read_csv('./data/french_words.csv')
 DELAY=3000
-word_list = data.to_dict(orient="records")
+try:    
+    data=pandas.read_csv('./data/to_learn.csv')
+except Exception:
+    data = pandas.read_csv("./data/french_words.csv")
+    word_list = data.to_dict(orient="records")
+
+
+else:
+    word_list = data.to_dict(orient="records")
 random_word_dict ={}
 
 
@@ -42,10 +48,12 @@ imageCard=canvas.create_image(400,263, image =card_back)
 language=canvas.create_text(400,150, text= "Title", font = ("Arial", 40, "italic"),fill="white")
 word=canvas.create_text(400,250, text= "Word", font = ("Arial", 65, "bold"))
 def next_card():
-    global random_word_dict, timer
+    global random_word_dict, timer, word_list
     window.after_cancel(id=timer)
+
     random_word_dict = random.choice(word_list)
     french_word = random_word_dict["French"]
+
     canvas.itemconfig(imageCard, image=card_front)
     canvas.itemconfig(language, text= "French", fill="black")
     canvas.itemconfig(word, text= french_word,fill= "black")
@@ -58,8 +66,16 @@ def flipcard():
     canvas.itemconfig(language, text="English",fill="black")
     canvas.itemconfig(word, text=english_word, fill = "white")
 
-    
-    
+def know_card():
+    try:
+        next_card()
+    except IndexError:
+        messagebox.showinfo(title="Congrats", message="You have reached the end of the flash card")
+    else:
+        word_list.remove(random_word_dict)
+        to_learn_data_frame = pandas.DataFrame(word_list)
+        
+        to_learn_data_frame.to_csv("./data/to_learn.csv", index=False)
     
 
 # WRONG BUTTON
@@ -67,7 +83,7 @@ wrong_button = Button(image=wrong_img,highlightthickness=0,command=next_card)
 wrong_button.grid(row=1,column=0,columnspan=1)
 
 # RIGHT BUTTON
-right_button = Button(image=right_img,highlightthickness=0,command=next_card)
+right_button = Button(image=right_img,highlightthickness=0,command=know_card)
 right_button.grid(row=1,column=1,columnspan=1)
 
 
