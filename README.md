c9924a27ee58112d097eb82932addf854f27f5b8
Nazif <nzfishrak60@gmail.com>
2022-09-04 11:25:17 -0700

    added readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# <strong>A Flash Card Learning App</strong>
+
+![demo1](images/demo1.png)
+![demo2](images/demo2.png)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
-# IMAGES
-card_back= PhotoImage(file="./images/card_back.png")
-card_front = PhotoImage(file="./images/card_front.png")
-right_img = PhotoImage(file="./images/right.png")
-wrong_img = PhotoImage(file= "./images/wrong.png")
-   
+"French"
